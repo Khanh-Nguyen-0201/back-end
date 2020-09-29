4911bb863e5c86ec6514646cf5382083de0c446e
Khanh <knguyen2@stevens.edu>
2020-09-28 20:23:02 -0400

    log4j added
    add log4j2 configs

--- src/main/java/com/example/ReactBackend/BackendApplication.java
+++ src/main/java/com/example/ReactBackend/BackendApplication.java
@@ -1,6 +1,9 @@
 package com.example.ReactBackend;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
+	private static final Logger logger = LogManager.getLogger(BackendApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -21,6 +25,14 @@ public class BackendApplication {
 				registry.addMapping("/*").allowedHeaders("*").allowedMethods("*");
 			}
 		};
+	}
+
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Warning Log");
+		logger.error("Error log");
+		logger.fatal("Fatal Error.");
 	}
 
 }
